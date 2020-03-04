47485101b7efa75cf3dcbdc1a3fa8d4226f0b0ae
7anminator <tanmay@happyness.net>
2020-03-04 16:34:23 +0530

    adds session controller and rubocop

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
+    @user = User.create(params.require(:user).permit(:username, :password))
+    session[:user_id] = @user.id
+    redirect_to '/welcome'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/login'
+  get 'sessions/welcome'
+  get 'users/new'
+  get 'users/create'
+  get 'login', to: 'sessions#new'
+  get 'welcome', to: 'sessions#welcome'
+  post 'login', to: 'sessions#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
